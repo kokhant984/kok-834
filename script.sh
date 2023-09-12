ee11c10dbe427307cd1feac86d24a3fbd1b1b4c6
KøKhantKø <125684956+kokhant984@users.noreply.github.com>
2023-09-12 22:17:29 +0630

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,24 +17,22 @@ none='\e[0m'
 
 sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/gcp_404/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
 echo "
-<h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
-</font></h3>
-<h3><font color='magenta'>--- ۩ PREMIUM SERVER ۩ ---
+<h3><font color='white'>▬▬▬▬▬▬MYANMAR▬▬▬▬▬▬
 </font></h3>
 
-<h3><font color='green'>--- ۩ SERVER BY ④ ⍤⃝🌻④ ۩ ---
+<h3><font color='yellow'>--- ۩ GOOGLE SERVER ۩ ---
 </font></h3>
 
-<h4><font color='cyan'>Telegram Channel >> https://t.me/Pmttg
+<h3><font color='green'>--- ۩ SERVER BY KOK834 ۩ ---
+</font></h3>
+
+<h4><font color='red'>--- ۩ https://t.me/KOKHANT834 ۩ ---
 </font></h4>
 
-<h4><font color='blue'>Telegram Channel >> https://t.me/nkka_404
-</font></h4>
-
-<h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
+<h3><font color='white'>▬▬▬▬▬▬BURMESE▬▬▬▬▬▬
 </font></h3>" | tee /etc/ssh/gcp_404 >/dev/null
-useradd "404" --shell=/bin/false -M
-echo "404:404" | chpasswd
+useradd "kok834" --shell=/bin/false -M
+echo "kok834:kok834" | chpasswd
 
 echo ""
 
